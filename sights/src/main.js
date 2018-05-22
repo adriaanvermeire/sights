8ff8e3679c018f642e212c652750a6730e57c29a
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-22 20:40:46 +0200

    set auth token in store when page gets refreshed

--- sights/src/main.js
+++ sights/src/main.js
@@ -1,17 +1,24 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import 'vue-awesome/icons';
 // import feather from 'vue-icon';
 import Icon from 'vue-awesome/components/Icon';
+import { setAuthorizationHeader } from '@/utils';
 import App from './App';
 import router from './router';
 import store from './store';
-
+// styles
 import './assets/scss/custom-bs.scss';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 // Vue.use(feather);
 Vue.component('icon', Icon);
+
+const token = localStorage.getItem('user-token');
+if (token) {
+  setAuthorizationHeader(token);
+}
 
 /* eslint-disable no-new */
 new Vue({
