2e1ab614106f2381b794773d47280c6e2e510025
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-22 10:40:02 +0200

    start using bootstrap-vue and fontawesome

--- sights/src/main.js
+++ sights/src/main.js
@@ -1,10 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+// import feather from 'vue-icon';
+import Icon from 'vue-awesome/components/Icon';
 import App from './App';
 import router from './router';
 import store from './store';
 
+import './assets/scss/custom-bs.scss';
+
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+// Vue.use(feather);
+Vue.component('icon', Icon);
 
 /* eslint-disable no-new */
 new Vue({
