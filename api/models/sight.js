e7bcd4a6520d6ad0456d047f03d8cd8beb55cfe9
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-23 15:14:56 +0200

    begin schema for sight

--- api/models/sight.js
+++ api/models/sight.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 // Sight schema
 const SightSchema = mongoose.Schema({
-
+  author: { type: ObjectId, ref: 'User' },
+  dataset: { type: String, required: true },
+  name: { type: String, required: true, index: true },
+  category: { type: ObjectId, ref: 'Category' },
+  charts: [{ type: ObjectId, ref: 'Chart' }],
 }, { timestamps: true });
 
 const Sight = module.exports = mongoose.model('Sight', SightSchema);
