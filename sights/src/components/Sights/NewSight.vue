0635a7d2ad68aaccabc3273f3b98d2f5d76247be
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-23 20:29:18 +0200

    add layout and js logic to send new sight to api

--- sights/src/components/Sights/NewSight.vue
+++ sights/src/components/Sights/NewSight.vue
@@ -1,4 +1,15 @@
 <template lang="html">
+<form class="" @submit.prevent="submitForm" method="post" enctype="multipart/form-data">
+  <input
+  type="text"
+  v-model.trim="sight.name"
+  v-validate="'required|min:5|alpha_num'">
+  <input
+  type="file"
+  @change="onFileSelected"
+  v-validate="'required|mimes:text/csv,application/json|size:5000'">
+  <button type="submit" name="button">Create Sight</button>
+</form>
 </template>
 
 <script>
@@ -12,6 +23,18 @@ export default {
         dataset: null,
       },
     };
+  },
+  methods: {
+    async submitForm() {
+      const fd = new FormData();
+      fd.append('dataset', this.sight.dataset, this.sight.dataset.name);
+      fd.append('name', this.sight.name);
+      const resp = await SightService.addSight(fd);
+      console.log(resp.data);
+    },
+    onFileSelected(e) {
+      this.sight.dataset = e.target.files[0];
+    },
   },
 };
 </script>
