fb7bc183459ef453a61b6e348bc875e1760d8d27
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-23 20:26:07 +0200

    add logic for uploading dataset and creating new sight

--- api/routes/sights.js
+++ api/routes/sights.js
@@ -13,5 +13,33 @@ const upload = multer({ dest });
 const Dataset = require('../models/dataset');
 const Sight = require('../models/sight');
 
+const routeMiddlewareNew = [
+  auth,
+  upload.single('dataset'),
+  validate(validation.sight),
+];
+router.post('/new', routeMiddlewareNew, (req, res) => {
+  // req.file is the `dataset` file
+  // req.body will hold the text fields, if there were any
+  const {
+    originalName, mimetype, filename, path, size,
+  } = req.file;
+
+  const newDataset = new Dataset({
+    originalName, mimetype, filename, path, size,
+  });
+
+  newDataset.save();
+
+  const newSight = new Sight({
+    dataset: newDataset._id,
+    category: '',
+    author: req.user._id,
+    name: req.body.name,
+  });
+
+  newSight.save();
+  res.send({ success: true, data: newSight });
+});
 
 module.exports = router;
