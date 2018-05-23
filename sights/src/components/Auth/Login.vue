ed059525501da87cebdaa150e90fbfb763dea0db
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-23 14:27:20 +0200

    implement frontend validation login

--- sights/src/components/Auth/Login.vue
+++ sights/src/components/Auth/Login.vue
@@ -6,22 +6,40 @@
         title='Login'
         >
         <b-form @submit.prevent="login">
-          <b-form-input
-          id='email'
-          v-model.trim="email"
-          type='email'
-          placeholder='Email'
-          class='mb-2'
-          ></b-form-input>
-          <b-form-input
-          id='password'
-          v-model.trim="password"
-          placeholder="Password"
-          class='mb-2'
-          type='password'
-          ></b-form-input>
-          <b-button :to="{ name: 'Register' }" variant='outline-primary'>Register</b-button>
-          <b-button type="submit" variant="primary">Login</b-button>
+          <b-form-row class='mb-2'>
+            <b-col cols="4" class='justify-content-end d-flex'>
+              <label for="email">Email</label>
+            </b-col>
+            <b-col cols="8">
+              <input
+              id='email' type='email' class='mb-2 form-control' name='email'
+              v-model.trim="email"
+              v-validate="'required|email'"
+              />
+              <b-form-invalid-feedback v-if="errors.has('email')"
+              class='text-right' :class='{ "d-block": errors.has("email")}'
+              >{{errors.first('email')}}</b-form-invalid-feedback>
+            </b-col>
+          </b-form-row>
+          <b-form-row class='mb-2'>
+            <b-col cols="4" class='justify-content-end d-flex'>
+              <label for="password">Password</label>
+            </b-col>
+            <b-col cols="8">
+              <input
+              id='password' class='mb-2 form-control' type='password' name='password'
+              v-model.trim="password"
+              v-validate="'required'"
+              />
+              <b-form-invalid-feedback v-if="errors.has('password')"
+              class='text-right' :class='{ "d-block": errors.has("password")}'
+              >{{errors.first('password')}}</b-form-invalid-feedback>
+            </b-col>
+          </b-form-row>
+          <span class='d-flex justify-content-end'>
+            <b-button :to="{ name: 'Register' }" variant='outline-primary'>Register</b-button>
+            <b-button type="submit" variant="primary" class='ml-2'>Login</b-button>
+          </span>
         </b-form>
 
       </b-card>
@@ -43,9 +61,13 @@ export default {
   methods: {
     login() {
       const { email, password } = this;
-      this.$store.dispatch(AUTH_REQUEST, { email, password }).then(() => {
+      this.errors.clear();
+      this.$store.dispatch(AUTH_REQUEST, { email, password }).then((res) => {
         this.$router.push('/');
-      });
+      })
+        .catch((err) => {
+          this.errors.add('password', err.data.msg, 'server');
+        });
     },
   },
 };
