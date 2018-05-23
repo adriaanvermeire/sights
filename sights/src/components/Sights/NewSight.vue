6b254958f21ba1e26858a81f7b9cbfd5ebfa6b32
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-23 20:27:12 +0200

    add new component + route for creating a new sight,

---
+++ sights/src/components/Sights/NewSight.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+</template>
+
+<script>
+import SightService from '@/services/SightService';
+
+export default {
+  data() {
+    return {
+      sight: {
+        name: '',
+        dataset: null,
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
