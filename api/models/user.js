2cc85a8ae1b6d017f0d1b3415fb561a697a4dd32
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-23 13:50:29 +0200

    change user schema to have unique values

--- api/models/user.js
+++ api/models/user.js
@@ -8,16 +8,19 @@ const UserSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    lowercase: true,
   },
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
-});
+}, { timestamps: true });
 
 const User = module.exports = mongoose.model('User', UserSchema);
 
