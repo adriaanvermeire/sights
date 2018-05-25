36dfe40ef4cd911239477fa38efa047e9eed7a3b
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-26 00:05:51 +0200

    add first steps for sight detail page

--- sights/src/components/Sights/SightDetail.vue
+++ sights/src/components/Sights/SightDetail.vue
@@ -1,4 +1,16 @@
 <template lang="html">
+<b-container fluid>
+  <b-row>
+    <b-col class='d-flex justify-content-between align-items-center'>
+      <span></span>
+      <h1>{{ sight.name }}</h1>
+        <icon name='user-circle' scale="3"></icon>
+    </b-col>
+  </b-row>
+  <b-row>
+    
+  </b-row>
+</b-container>
 </template>
 
 <script>
