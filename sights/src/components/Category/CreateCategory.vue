5ac7f56c970fcb42fd5c381ab7e6271becc2c783
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-25 23:59:23 +0200

    implement small form for creating category

--- sights/src/components/Category/CreateCategory.vue
+++ sights/src/components/Category/CreateCategory.vue
@@ -1,10 +1,38 @@
 <template lang="html">
+<b-form @submit='submit'>
+   <b-form-input v-model="name"
+                type="text"
+                placeholder="Enter the category"
+  ></b-form-input>
+  <b-button variant="primary" type="submit">
+      Create Category
+  </b-button>
+</b-form>
 </template>
 
 <script>
 import CategoryService from '@/services/CategoryService';
 
 export default {
+  data() {
+    return {
+      name: '',
+    };
+  },
+  methods: {
+    async submit() {
+      console.log(this.name);
+      if (this.name) {
+        const res = (await CategoryService.addCategory({ name: this.name })).data;
+        console.log(res);
+        if (res.success) {
+          this.$router.push({ name: 'Home' });
+        } else {
+          console.log('Failed to create category');
+        }
+      }
+    },
+  },
 };
 </script>
 
