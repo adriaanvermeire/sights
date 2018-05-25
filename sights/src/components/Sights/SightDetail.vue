53cee5fe71616baa08eb235c282521896da9774e
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-25 23:54:23 +0200

    use service route to get sight

--- sights/src/components/Sights/SightDetail.vue
+++ sights/src/components/Sights/SightDetail.vue
@@ -1,7 +1,19 @@
 <template lang="html">
 </template>
+
 <script>
+import SightService from '@/services/SightService';
+
 export default {
+  data() {
+    return {
+      sight: {},
+    };
+  },
+  async mounted() {
+    const id = this.$route.params.id;
+    this.sight = (await SightService.getSight(id)).data.sight;
+  },
 };
 </script>
 
