a694f690bb21bea0b3010e732dd6c7f2bc0a39cc
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-27 23:55:19 +0200

    add currently editing sight to store, add actions and mutations

--- sights/src/store/actions/user.js
+++ sights/src/store/actions/user.js
@@ -1,3 +1,5 @@
 export const USER_REQUEST = 'USER_REQUEST';
 export const USER_SUCCESS = 'USER_SUCESS';
 export const USER_ERROR = 'USER_ERROR';
+export const USER_SIGHT = 'USER_SIGHT';
+export const USER_NOSIGHT = 'USER_NOSIGHT';
