272147c40733f256abb2db45baa528b7d04aa56c
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-28 23:54:32 +0200

    add method for counting occurences and calculating various metadata

--- api/utils/index.js
+++ api/utils/index.js
@@ -30,3 +30,26 @@ module.exports.detectType = function detectType(samples) {
   }
   return mostFrequent(results);
 };
+
+module.exports.countOccurences = function countOccurences(array) {
+  const result = {
+    total: array.length,
+    data: array,
+    counts: {},
+    uniqueValues: 0,
+    isVolatile: false,
+    isSteady: false,
+  };
+  for (let i = 0; i < array.length; i += 1) {
+    const el = array[i];
+    if (Object.prototype.hasOwnProperty.call(result.counts, el)) {
+      result.counts[el] += 1;
+    } else {
+      result.counts[el] = 1;
+    }
+  }
+  result.uniqueValues = Object.keys(result.counts).length;
+  if ((result.uniqueValues / result.total) > 0.6) result.isVolatile = true;
+  else if (result.uniqueValues / result.total <= 0.1) result.isSteady = true;
+  return result;
+};
