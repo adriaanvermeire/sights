0f6f21087f422b74c4d69051348daee838f95d44
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-06-04 23:31:34 +0200

    remake navbar with plain bootstrap

--- sights/src/components/Partials/Navbar.vue
+++ sights/src/components/Partials/Navbar.vue
@@ -1,38 +1,52 @@
 <template lang="html">
-<b-navbar variant="primary" toggleable="sm">
-  <b-navbar-brand :to="{ name: 'Home' }">
+<div class="navigation navbar navbar-expand bg-primary d-flex justify-content-between align-items-center">
+  <router-link :to='{ name: "Home" }' class='navbar-brand'>
     <img class='logo' src="@/assets/graphics/logo-sights-inverse-notext.png" alt="">
     Sights
-  </b-navbar-brand>
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-  <b-collapse is-nav id="nav_collapse">
-    <b-navbar-nav class='ml-auto'>
-      <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
-      <b-nav-item :to="{ name: 'Explore' }">Explore</b-nav-item>
-      <b-nav-item v-if="isAuthenticated" :to="{ name: 'MySights' }">My Sights</b-nav-item>
-      <b-nav-item v-if="isAuthenticated" :to="{ name: 'NewSight' }">
-        <icon name="plus" color='white'></icon>
-      </b-nav-item>
-      <b-nav-item-dropdown v-if="isAuthenticated" right>
-        <slot name='button-content'>
-          <icon name="user" color='white'></icon><span class="sr-only">Profile</span>
-        </slot>
-        <b-dropdown-item href="#">Profile</b-dropdown-item>
-        <b-dropdown-item href="#">Settings</b-dropdown-item>
-        <b-dropdown-divider></b-dropdown-divider>
-        <b-dropdown-item @click.prevent='logout'>Logout</b-dropdown-item>
-      </b-nav-item-dropdown>
-      <b-nav-item v-if="!isAuthenticated" :to="{ name: 'Register' }">
-        <strong>Register</strong>
-      </b-nav-item>
-      <b-nav-item v-if="!isAuthenticated" :to="{ name: 'Login' }">Login</b-nav-item>
-    </b-navbar-nav>
-  </b-collapse>
-</b-navbar>
+  </router-link>
+  <div class="collapse navigation-links d-flex align-items-center">
+    <ul class='navbar-nav d-flex m-0 p-0 d-flex align-items-center'>
+      <li class="nav-item">
+        <router-link class='nav-link':to='{ name: "Home"}'>Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class='nav-link':to='{ name: "Explore"}'>Explore</router-link>
+      </li>
+      <template v-if='isAuthenticated'>
+      <li class="nav-item">
+        <router-link class='nav-link':to='{ name: "MySights"}'>My Sights</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class='nav-link':to='{ name: "NewSight"}'>Add Sight</router-link>
+      </li>
+      <li class="nav-item pr-0 dropdown">
+        <a href="#" class='nav-link dropdown-toggle' id='profileDropdown' role='button' data-toggle='dropdown' aria-haspopup="true" aria-expanded="false">
+          <avatar :username="username" :size='40'></avatar>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="profileDropdown">
+          <router-link class="dropdown-item" :to="{ name: 'Profile' }">Profile</router-link>
+          <router-link class="dropdown-item" :to="{ name: 'Profile' }">Settings</router-link>
+          <div class="dropdown-divider"></div>
+          <a @click.prevent='logout' class="dropdown-item" href="#">Logout</a>
+        </div>
+      </li>
+      </template>
+      <template v-else>
+      <li class="nav-item">
+        <router-link class='nav-link' :to='{ name: "Register"}'>Register</router-link>
+      </li>
+      <li class="nav-item pr-0">
+        <router-link class='nav-link' :to='{ name: "Login"}'>Login</router-link>
+      </li>
+      </template>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
 import { AUTH_LOGOUT } from '@/store/actions/auth';
+import Avatar from 'vue-avatar';
 
 export default {
   data() {
@@ -49,12 +63,18 @@ export default {
     isAuthenticated() {
       return this.$store.getters.isAuthenticated;
     },
+    username() {
+      return this.$store.state.user.profile.username;
+    },
+  },
+  components: {
+    Avatar,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.navbar{
+.navigation{
   box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
   padding-top: 0;
   padding-bottom: 0;
@@ -68,6 +88,15 @@ export default {
   .logo {
     height: 60px;
   }
-
+  .b-nav-dropdown {
+    a{
+      span{
+color: white;
+      }
+    }
+  }
+}
+.dropdown-toggle::after {
+    display:none;
 }
 </style>
