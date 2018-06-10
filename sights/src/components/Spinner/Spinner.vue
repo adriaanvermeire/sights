48bde69503c47b253be9f28eb22a20249cf41d14
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-06-10 09:40:58 +0200

    Add spinner component

---
+++ sights/src/components/Spinner/Spinner.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="spinner">
+    <spinner color='#09eba7'/>
+    <span :v-model='text'></span>
+</div>
+</template>
+
+<script>
+import Spinner from 'vue-spinner-component/src/Spinner';
+
+export default {
+  props: ['text'],
+  components: {
+    Spinner,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
