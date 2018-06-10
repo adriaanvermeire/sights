2d6143185cd6191bedb1bdb30c23d060d21baabb
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-06-10 11:17:52 +0200

    add animated ellipsis

--- sights/src/components/Spinner/Spinner.vue
+++ sights/src/components/Spinner/Spinner.vue
@@ -1,7 +1,7 @@
 <template>
-<div id="spinner">
-    <spinner color='#09eba7'/>
-    <span :v-model='text'></span>
+<div id="spinner" class='d-flex align-items-center flex-column mt-5'>
+    <spinner color='#09eba7' size='150' :depth='10'/>
+    <h5 class='mt-2'>{{text}}</h5>
 </div>
 </template>
 
@@ -17,5 +17,26 @@ export default {
 </script>
 
 <style scoped>
+#spinner h5:after {
+    position: absolute;
+    overflow: hidden;
+    display: inline-block;
+    vertical-align: bottom;
+    -webkit-animation: ellipsis steps(4,end) 900ms infinite;
+    animation: ellipsis steps(4,end) 900ms infinite;
+    content: "\2026"; /* ascii code for the ellipsis character */
+    width: 0px;
+}
 
+@keyframes ellipsis {
+    to {
+        width: 1.25em;
+    }
+}
+
+@-webkit-keyframes ellipsis {
+    to {
+        width: 1.25em;
+    }
+}
 </style>
