f94792360ae390f98f0d12ddc8d3d5341cf1ddc1
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-06-10 23:23:49 +0200

    refactor loadSights, use sightCard comp and show categories

--- sights/src/components/Sights/Explore.vue
+++ sights/src/components/Sights/Explore.vue
@@ -1,43 +1,69 @@
 <template lang="html">
-  <div id="explore">
-    <ul>
-      <li v-for="sight in sights" :key='sight._id'>
-        <router-link
-        :to="{
-          name: 'SightDetail',
-          params: { id: sight._id || 1 }
-          }" class='text-dark'
-        >{{ sight.name }}</router-link>
-      </li>
+  <div id="explore" class='container-fluid mx-auto'>
+    <ul id='categories' class='d-flex'>
+      <li class='text-muted'>Categories:</li>
+      <li class="category nav-item"><button class="btn btn-outline-dark">Test</button></li>
     </ul>
-    <p v-show="!sights">
-      There are no Sights to be found!
-    </p>
+    <template v-show='!loading'>
+      <div class="sights-wrapper row">
+        <sight-card v-for='sight in sights' :key='sight._id' :sight='sight'/>
+        <p v-if="!sights.length">
+          There are no Sights to be found!
+        </p>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import SightService from '@/services/SightService';
+import CategoryService from '@/services/CategoryService';
+import SightCard from '@/components/Card/SightCard';
 
 export default {
   data() {
     return {
       sights: [],
+      loading: true,
+      categories: [],
     };
   },
   methods: {
-    loadSights() {
-      SightService.getFeaturedSights()
-        .then(res => res.data)
-        .then((data) => { this.sights = data.sights; })
-        .catch(e => console.log(e));
+    async loadSights() {
+      try {
+        const response = (await SightService.getFeaturedSights()).data;
+        this.sights = response.sights;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async loadCategories() {
+      try {
+        const response = (await CategoryService.all()).data;
+        this.categories = response.categories;
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
-  created() {
-    this.loadSights();
+  async created() {
+    await this.loadSights();
+    await this.loadCategories();
+    this.loading = false;
+  },
+  components: {
+    SightCard,
   },
 };
 </script>
 
 <style lang="scss" scoped>
+#categories {
+  list-style-type: none;
+  align-items: center;
+
+  li {
+    margin-right: 1rem;
+  }
+}
 </style>
