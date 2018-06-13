4d376f11d74a2fb881f00f4d89d1208c256216e4
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-06-13 15:21:42 +0200

    add algolia instantsearch to explore page and move categories and search into own component

---
+++ sights/src/components/Explore/Explore.vue
@@ -0,0 +1,95 @@
+<template lang="html">
+  <div id="explore" class='container-fluid mx-auto px-5 pb-5'>
+    <ais-index
+          app-id="OQTWW0B4H3"
+          api-key="97e17abaac2e3a6bd2677c5176b7ec7d"
+          index-name="sights"
+          :query-parameters="categoryFilter"
+      >
+      <search-bar @search="searchCategory($event)"/>
+      <ais-results>
+        <template slot-scope="{ result }">
+          <sight-card :key='result.objectID' :sight='result'/>
+        </template>
+      </ais-results>
+    </ais-index>
+  </div>
+</template>
+
+<script>
+import SightService from '@/services/SightService';
+import SightCard from '@/components/Card/SightCard';
+import SearchBar from './SearchBar';
+
+export default {
+  data() {
+    return {
+      sights: [],
+      loading: true,
+      filterCategory: '',
+    };
+  },
+  methods: {
+    async loadSights(filter) {
+      try {
+        const response = (await SightService.getSights(filter)).data;
+        this.sights = response.sights;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    searchCategory(payload) {
+      this.filterCategory = payload.name;
+    },
+  },
+  computed: {
+    categoryFilter() {
+      const filter = { filters: '' };
+      if (this.filterCategory) {
+        filter.filters = `category:${this.filterCategory}`;
+      }
+      return filter;
+    },
+  },
+  async created() {
+    await this.loadSights();
+    this.loading = false;
+  },
+  components: {
+    SightCard, SearchBar,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.ais-results {
+  display: grid;
+  grid-gap: 1rem;
+  justify-content: space-between;
+}
+</style>
+
+<style lang="scss">
+@media screen and (max-width: 40em) {
+  .ais-results {
+    grid-template-columns: repeat(auto-fill, 100%);
+  }
+}
+@media screen and (min-width: 40em) {
+  .ais-results {
+    grid-template-columns: repeat(auto-fill, 48%);
+  }
+}
+
+@media screen and (min-width: 60em) {
+  .ais-results {
+      grid-template-columns: repeat(auto-fill, minmax(19%, 32%));
+  }
+}
+
+@media screen and (min-width: 80em) {
+  .ais-results {
+    grid-template-columns: repeat(auto-fill, minmax(0, 19%));
+  }
+}
+</style>
